a0aeaed5e4291e57e543fe8aa56649d9881f1d40
Kedar Agate <kedar.agate@gmail.com>
2024-12-04 07:20:31 +0530

    developing for 2 digits

--- script.js
+++ script.js
@@ -27,9 +27,11 @@ function checkNumberOfDigits(num) {
   }
   return count;
 }
+function checkPlaceValue(num) {}
+
 function getRomanValue(obj, value) {
   let objectKeys = Object.keys(obj);
-  console.log(objectKeys);
+
   let key = objectKeys.find((key) => {
     return obj[key] === value;
   });
@@ -37,21 +39,50 @@ function getRomanValue(obj, value) {
 }
 
 convertBtn.addEventListener("click", () => {
+  output.textContent = "";
+
   let inputNumber = Number(input.value);
   let numOfDigits = checkNumberOfDigits(inputNumber);
 
-  if (getRomanValue(romanToArebicObj, inputNumber)) {
-    output.textContent = getRomanValue(romanToArebicObj, inputNumber);
-  } else {
-    if (numOfDigits === 1) {
-      if (inputNumber <= 3) {
-        let count = 1;
-        while (count <= inputNumber) {
-          output.textContent =
-            output.textContent + getRomanValue(romanToArebicObj, 1);
-          count++;
-        }
-      }
+  if (numOfDigits === 1) {
+    if (getRomanValue(romanToArebicObj, inputNumber)) {
+      output.textContent = getRomanValue(romanToArebicObj, inputNumber);
+    } else {
+      printRomanValuesForSingleDigitNumber(inputNumber, output);
+    }
+  } else if (numOfDigits === 2) {
+    if (getRomanValue(romanToArebicObj, inputNumber)) {
+      output.textContent = getRomanValue(romanToArebicObj, inputNumber);
+    } else {
+      let numberAtOncePlace = inputNumber % 10;
+      console.log(numberAtOncePlace);
+      output.textContent = getRomanValue(
+        romanToArebicObj,
+        inputNumber - numberAtOncePlace
+      );
+
+      printRomanValuesForSingleDigitNumber(numberAtOncePlace, output);
     }
   }
 });
+
+function printRomanValuesForSingleDigitNumber(number, HTMLElement) {
+  console.log(HTMLElement);
+  if (number <= 3) {
+    let count = 1;
+    while (count <= number) {
+      HTMLElement.textContent =
+        HTMLElement.textContent + getRomanValue(romanToArebicObj, 1);
+      count++;
+    }
+  } else if (number >= 6 && number <= 8) {
+    let count = 6;
+    let tempNum5 = getRomanValue(romanToArebicObj, 5);
+    while (count <= number) {
+      HTMLElement.textContent =
+        HTMLElement.textContent + tempNum5 + getRomanValue(romanToArebicObj, 1);
+      count++;
+      tempNum5 = "";
+    }
+  }
+}
